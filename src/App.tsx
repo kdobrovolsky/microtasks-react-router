303adcb9e7b56b78477a7e4311334dfe03ccb9e4
kdobrovolsky <kdobrovolsky02@gmail.com>
2025-08-12 20:02:28 +0300

    refactor project

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,44 @@
-import { Navigate, NavLink, Route, Routes } from "react-router-dom";
-import { PageOne } from "./components/pages/PageOne";
-import { PageThree } from "./components/pages/PageThree";
-import { PageTwo } from "./components/pages/PageTwo";
+import { NavLink, Route, Routes} from "react-router-dom";
+import {PageOne} from "./components/pages/PageOne";
+import {PageThree} from "./components/pages/PageThree";
+import {PageTwo} from "./components/pages/PageTwo";
 import styles from "./components/Site.module.css";
-import { Error404 } from "./components/pages/Error404";
+import {Error404} from "./components/pages/Error404";
 import './index.css'
-import styled from "styled-components";
 
-export const App = ()=> {
-  return (
-    <div>
-      <div className={styles.header}><h1>HEADER</h1></div>
-      <div className={styles.body}>
-        <div className={styles.nav}>
-          <div><NavLink to={'/page1'} className={({isActive}) => isActive ? 'activeNavLink': 'navLink'} >Page1</NavLink> </div>
-          <div><NavLink to={'/page2'} className={({isActive}) => isActive ? 'activeNavLink': 'navLink'}>Page2</NavLink></div>
-          <div><NavLink to={'/page3'} className={({isActive}) => isActive ? 'activeNavLink': 'navLink'}>Page3</NavLink></div>
-         </div>
-        <div className={styles.content}>
-          Здесь будут кроссовки
-          <Routes>
-            <Route path="/page1" element={<PageOne/>} />
-            <Route path="/page2" element={<PageTwo/>}/>
-            <Route path="/page3" element={<PageThree/>}/>
-            <Route path="/error404" element={<Error404/>}/>
+const PATH = {
+    PAGE1: '/page1',
+    PAGE2: '/page2',
+    PAGE3: '/page3',
+    PAGE4: '/error404',
+} as const
 
-            <Route path="*" element={<Navigate to={'/error404'}/>}/>
-            <Route path="/" element={<Navigate to={'/page1'}/>}/>
-          </Routes>
+const getNavLinkClass = ({isActive}: { isActive: boolean }) =>
+    isActive ? 'activeNavLink' : 'navLink';
+export const App = () => {
+    return (
+        <div>
+
+            <div className={styles.header}><h1>HEADER</h1></div>
+            <div className={styles.body}>
+                <div className={styles.nav}>
+                    <div><NavLink to={PATH.PAGE1}>Page1</NavLink></div>
+                    <div><NavLink to={PATH.PAGE2} className={getNavLinkClass}>Page2</NavLink></div>
+                    <div><NavLink to={PATH.PAGE3} className={getNavLinkClass}>Page3</NavLink></div>
+                </div>
+                <div className={styles.content}>
+                    Здесь будут кроссовки
+                    <Routes>
+                        <Route path={PATH.PAGE1} element={<PageOne/>}/>
+                        <Route path={PATH.PAGE2} element={<PageTwo/>}/>
+                        <Route path={PATH.PAGE3} element={<PageThree/>}/>
+                        <Route path={PATH.PAGE4} element={<Error404/>}/>
+                        <Route path={"/*"} element={<Error404/>}/>
+
+                    </Routes>
+                </div>
+            </div>
+            <div className={styles.footer}>abibas 2023</div>
         </div>
-      </div>
-      <div className={styles.footer}>abibas 2023</div>
-    </div>
-  );
+    );
 }
-
-const NavWrapper=styled.div`
-    margin-left: 10px;
-    font-size: 20px;
-    &>a{
-        text-decoration: none;
-        color: #1e3786;
-    }
-    &>a.active{
-        text-decoration: none;
-        color: #03eaff;
-    }
-    &>a:hover{
-        color: steelblue;
-    }
-`
